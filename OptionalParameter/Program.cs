6f18a660994d401e41bd92dbb9e3f6b5e1170233
tuilatin <nguyentin1763@gmail.com>
2025-02-27 20:52:37 +0700

    Run code bai OptionalParameter

---
+++ OptionalParameter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    // Phương thức tính lương với tham số tùy chọn
+    static double TinhLuong(double luongCoBan, double thuong = 0, double phuCap = 500000)
+    {
+        return luongCoBan + thuong + phuCap;
+    }
+
+    static void Main()
+    {
+        // Gọi hàm với các cách truyền tham số khác nhau
+        Console.WriteLine("Lương với chỉ lương cơ bản: " + TinhLuong(10000000));
+        Console.WriteLine("Lương với lương cơ bản và thưởng: " + TinhLuong(10000000, 2000000));
+        Console.WriteLine("Lương với cả ba tham số: " + TinhLuong(10000000, 2000000, 1000000));
+    }
+}
